0822b69b280ac8011523d26b5f5593eb03c463d1
muhammad_adil <adilbughio@gmail.com>
2024-05-29 17:08:33 +0500

    Add files via upload

---
+++ SIS_QUERY.sql
@@ -0,0 +1,341 @@
+--1. *Retrieve all student names:*
+    
+   SELECT Name FROM Students;
+   
+
+--2. *Retrieve all course names and their credits:*
+    
+   SELECT Course_Name, Credits FROM Courses;
+   
+
+--3. *Retrieve all enrolled students name  in a specific course id=2:*
+   
+   SELECT Students.Name FROM Students
+   JOIN Enrollments ON Students.Student_ID = Enrollments.Student_ID
+   WHERE Enrollments.Course_ID = 2;
+   
+
+--4. *Retrieve all courses taken by a specific student:*
+    
+   SELECT Courses.Course_Name FROM Courses
+   JOIN Enrollments ON Courses.Course_ID = Enrollments.Course_ID
+   WHERE Enrollments.Student_ID = 3;
+   
+
+--5. *Retrieve the total number of enrolled students in each course:*
+    
+   SELECT Course_ID, COUNT(Student_ID) AS Enrolled_Students FROM Enrollments
+   GROUP BY Course_ID;
+   
+
+--6. *Retrieve the names and emails of students who have enrolled in a specific course:*
+    
+   SELECT Students.Name, Students.Email FROM Students
+   JOIN Enrollments ON Students.Student_ID = Enrollments.Student_ID
+   WHERE Enrollments.Course_ID = 3;
+   
+
+--7. *Retrieve the average grade of each student:*
+    
+   SELECT Student_ID, AVG(Grade) AS Average_Grade FROM Grades
+   GROUP BY Student_ID;
+   
+
+--8. *Retrieve all students along with the number of courses they have received grades for:*
+  
+    SELECT Students.Name, COUNT(DISTINCT Grades.Course_ID) AS Graded_Courses FROM Students
+    LEFT JOIN Grades ON Students.Student_ID = Grades.Student_ID
+    GROUP BY Students.Name;
+    
+
+   
+
+--9. *Retrieve all students who have not attended any classes:*
+    
+   SELECT Students.Name FROM Students
+   LEFT JOIN Attendance ON Students.Student_ID = Attendance.Student_ID
+   WHERE Attendance.Student_ID IS NULL;
+   
+
+--10. *Retrieve the highest grade in each course:*
+     
+    SELECT Course_ID, MAX(Grade) AS Highest_Grade FROM Grades
+    GROUP BY Course_ID;
+    
+
+--11. *Retrieve all courses with more than 3 credits:*
+     
+    SELECT * FROM Courses WHERE Credits > 3;
+    
+
+--12. *Retrieve all students who have an email address ending with '@example.com':*
+     
+    SELECT * FROM Students WHERE Email LIKE '%@example.com';
+    
+
+--13. *Retrieve all courses with no enrolled students:*
+     
+    SELECT Courses.Course_Name FROM Courses
+    LEFT JOIN Enrollments ON Courses.Course_ID = Enrollments.Course_ID
+    WHERE Enrollments.Course_ID IS NULL;
+    
+
+--14. *Retrieve the total number of students in the Students table:*
+     
+    SELECT COUNT(*) AS Total_Students FROM Students;
+    
+
+--15. *Retrieve the student who has the highest grade in a specific course:*
+     
+    SELECT Students.Name FROM Students
+    JOIN Grades ON Students.Student_ID = Grades.Student_ID
+    WHERE Grades.Course_ID = 1
+    ORDER BY Grades.Grade DESC
+   
+
+--16. *Retrieve all students and their corresponding course enrollments:*
+     
+    SELECT Students.Name, Courses.Course_Name FROM Students
+    JOIN Enrollments ON Students.Student_ID = Enrollments.Student_ID
+    JOIN Courses ON Enrollments.Course_ID = Courses.Course_ID;
+    
+
+--17. *Retrieve all courses with their respective departments:*
+     
+    SELECT Course_Name, Department FROM Courses;
+    
+
+--18. *Retrieve all courses with their enrollment counts:*
+     
+    SELECT Courses.Course_Name, COUNT(Enrollments.Enrollment_ID) AS Enrollment_Count FROM Courses
+    LEFT JOIN Enrollments ON Courses.Course_ID = Enrollments.Course_ID
+    GROUP BY Courses.Course_Name;
+    
+
+--19. *Retrieve all students who have attended a class on a specific date:*
+     
+    SELECT Students.Name FROM Students
+    JOIN Attendance ON Students.Student_ID = Attendance.Student_ID
+    WHERE Attendance.Date = '2022-01-12';
+    
+
+--20. *Retrieve the average grade for a specific student in all their courses:*
+     
+    SELECT AVG(Grade) AS Average_Grade FROM Grades
+    WHERE Student_ID = 7;
+    
+
+--21. *Retrieve all students who have the same phone number:*
+     
+    SELECT Phone, COUNT(*) AS Count FROM Students
+    GROUP BY Phone HAVING COUNT(*) > 1;
+    
+
+--22 Retrieve the total number of assignments graded per course:*
+ 
+    SELECT Course_ID, COUNT(Grade_ID) AS Total_Assignments FROM Grades
+    GROUP BY Course_ID;
+     
+ --23. *Retrieve the highest grade obtained by each student:*
+     
+    SELECT Student_ID, MAX(Grade) AS Highest_Grade FROM Grades
+    GROUP BY Student_ID;
+    
+
+--24. *Retrieve all courses with enrollment dates after a specific date:*
+     
+    SELECT Courses.Course_Name FROM Courses
+    JOIN Enrollments ON Courses.Course_ID = Enrollments.Course_ID
+    WHERE Enrollments.Enrollment_Date > '2021-02-15';
+    
+
+--25. *Retrieve all students who have a grade less than 80 in any course:*
+     
+    SELECT DISTINCT Students.Name FROM Students
+    JOIN Grades ON Students.Student_ID = Grades.Student_ID
+    WHERE Grades.Grade < 80;
+    
+
+--26. *Retrieve the number of students enrolled in each course department:*
+     
+    SELECT Courses.Department, COUNT(Students.Student_ID) AS Enrolled_Students FROM Courses
+    JOIN Enrollments ON Courses.Course_ID = Enrollments.Course_ID
+    JOIN Students ON Enrollments.Student_ID = Students.Student_ID
+    GROUP BY Courses.Department;
+    
+
+--27. Retrieve the total number of classes attended by each student:*
+    
+    SELECT Students.Name, COUNT(Attendance.Attendance_ID) AS Classes_Attended FROM Students
+    JOIN Attendance ON Students.Student_ID = Attendance.Student_ID
+    GROUP BY Students.Name;
+     
+	     
+
+--28. *Retrieve all students who have not been graded in any course:*
+     
+    SELECT Students.Name FROM Students
+    LEFT JOIN Grades ON Students.Student_ID = Grades.Student_ID
+    WHERE Grades.Student_ID IS NULL;
+    
+
+--29. *Retrieve the number of courses each student is enrolled in:*
+     
+    SELECT Student_ID, COUNT(Course_ID) AS Enrolled_Courses FROM Enrollments
+    GROUP BY Student_ID;
+    
+
+--30. *Retrieve all students who have attended a class with a specific attendance status:*
+     
+    SELECT Students.Name FROM Students
+    JOIN Attendance ON Students.Student_ID = Attendance.Student_ID
+    WHERE Attendance.Attendance_Status = '<status>';
+    
+
+--31. *Retrieve the average grade for each assignment for a specific course:*
+     
+    SELECT Assignment_Name, AVG(Grade) AS Average_Grade FROM Grades
+    WHERE Course_ID = 3
+    GROUP BY Assignment_Name;
+    
+
+--32. Retrieve all courses that have never been assigned a grade:*
+   
+    SELECT Courses.Course_Name FROM Courses
+    LEFT JOIN Grades ON Courses.Course_ID = Grades.Course_ID
+    WHERE Grades.Course_ID IS NULL;
+    
+
+--33. *Retrieve the course with the highest enrollment:*
+     
+    SELECT Course_ID, COUNT(Student_ID) AS Enrolled_Students FROM Enrollments
+    GROUP BY Course_ID
+    ORDER BY Enrolled_Students DESC
+    
+    
+
+--34. *Retrieve the students who have enrolled in all available courses:*
+     
+    SELECT Students.Name FROM Students
+    WHERE NOT EXISTS (
+        SELECT * FROM Courses
+        WHERE NOT EXISTS (
+            SELECT * FROM Enrollments
+            WHERE Enrollments.Student_ID = Students.Student_ID
+            AND Enrollments.Course_ID = Courses.Course_ID
+        )
+    );
+    
+
+--35. *Retrieve the course(s) with the lowest average grade:*
+     
+    SELECT Course_ID, AVG(Grade) AS Average_Grade FROM Grades
+    GROUP BY Course_ID
+    ORDER BY Average_Grade ASC
+    
+    
+
+--36. *Retrieve the students who have not attended any class in a specific course:*
+     
+    SELECT Students.Name FROM Students
+    LEFT JOIN Attendance ON Students.Student_ID = Attendance.Student_ID
+    WHERE Attendance.Course_ID = 4 AND Attendance.Student_ID IS NULL;
+    
+
+--37. *Retrieve the course with the most assignments:*
+     
+    SELECT Course_ID, COUNT(Assignment_Name) AS Assignment_Count FROM Grades
+    GROUP BY Course_ID
+    ORDER BY Assignment_Count DESC
+    
+    
+
+--38. *Retrieve the courses where all students have attended classes:*
+     
+    SELECT Course_ID FROM Enrollments
+    GROUP BY Course_ID
+    HAVING COUNT(DISTINCT Student_ID) = (SELECT COUNT(*) FROM Students);
+    
+
+--39.Retrieve the names of students along with the total number of grades they have received:*
+  
+    SELECT Students.Name, COUNT(Grades.Grade_ID) AS Total_Grades FROM Students
+    LEFT JOIN Grades ON Students.Student_ID = Grades.Student_ID
+    GROUP BY Students.Name;
+         
+
+--40.Retrieve the names of students whose names contain a specific substring (e.g., 'John') using LIKE:*
+    
+    SELECT Name FROM Students
+    WHERE Name LIKE '%John%';     
+    
+
+--41. *Retrieve the students who have the same address:*
+     
+    SELECT Address, COUNT(*) AS Count FROM Students
+    GROUP BY Address HAVING COUNT(*) > 1;
+    
+
+--42. Retrieve the total number of students who have received grades within a specified range (e.g., between 80 and 90) using BETWEEN:*
+    
+    SELECT COUNT(DISTINCT Student_ID) AS Students_In_Range FROM Grades
+    WHERE Grade BETWEEN 80 AND 90;
+    
+
+--43. *Retrieve the courses with the highest average grade:*
+     
+    SELECT Course_ID, AVG(Grade) AS Average_Grade FROM Grades
+    GROUP BY Course_ID
+    ORDER BY Average_Grade DESC
+   
+    
+
+--44. *Retrieve the students who have the same name:*
+     
+    SELECT Name, COUNT(*) AS Count FROM Students
+    GROUP BY Name HAVING COUNT(*) > 1;
+    
+
+--45. 68. Retrieve the course with the highest average grade using AVG and JOIN:*
+   
+    SELECT top 1 Courses.Course_Name, AVG(Grades.Grade) AS Average_Grade FROM Courses
+    JOIN Grades ON Courses.Course_ID = Grades.Course_ID
+    GROUP BY Courses.Course_Name
+    ORDER BY Average_Grade DESC
+   
+    
+
+--46. *Retrieve the courses with no attendance recorded:*
+     
+    SELECT Courses.Course_Name FROM Courses
+    LEFT JOIN Attendance ON Courses.Course_ID = Attendance.Course_ID
+    WHERE Attendance.Course_ID IS NULL;
+    
+
+--47. count total number of students in our system.
+
+SELECT COUNT(students.name) as Total_students
+FROM students
+
+
+--48. *Retrieve the courses where all enrolled students have passed (grade >= 60):*
+     
+    SELECT Course_ID FROM Enrollments
+    WHERE Course_ID NOT IN (
+        SELECT Course_ID FROM Grades
+        WHERE Grade < 60
+    );
+    
+
+--49. *Retrieve the students who have attended the least number of classes:*
+     
+    SELECT Student_ID, COUNT(Attendance_ID) AS Class_Count FROM Attendance
+    GROUP BY Student_ID
+    ORDER BY Class_Count ASC
+   
+
+--50. *Retrieve the students who have not enrolled in any course:*
+     
+    SELECT Students.Name FROM Students
+    LEFT JOIN Enrollments ON Students.Student_ID = Enrollments.Student_ID
+    WHERE Enrollments.Student_ID IS NULL;
---
+++ SIS_TABLES.sql
@@ -0,0 +1,121 @@
+
+CREATE DATABASE SIS;
+
+USE SIS;
+
+--CREATING TABLES ---
+
+CREATE TABLE Students (
+  Student_ID INT PRIMARY KEY,
+  Name VARCHAR(50),
+  Email VARCHAR(50),
+  Phone VARCHAR(20),
+  Address VARCHAR(100)
+);
+
+CREATE TABLE Courses (
+  Course_ID INT PRIMARY KEY,
+  Course_Name VARCHAR(50),
+  Credits INT,
+  Department VARCHAR(50)
+);
+
+CREATE TABLE Enrollments (
+  Enrollment_ID INT PRIMARY KEY,
+  Student_ID INT,
+  Course_ID INT,
+  Enrollment_Date DATE,
+  FOREIGN KEY (Student_ID) REFERENCES Students(Student_ID),
+  FOREIGN KEY (Course_ID) REFERENCES Courses(Course_ID)
+);
+
+CREATE TABLE Grades (
+  Grade_ID INT PRIMARY KEY,
+  Student_ID INT,
+  Course_ID INT,
+  Assignment_Name VARCHAR(50),
+  Grade INT,
+  FOREIGN KEY (Student_ID) REFERENCES Students(Student_ID),
+  FOREIGN KEY (Course_ID) REFERENCES Courses(Course_ID)
+);
+
+CREATE TABLE Attendance (
+  Attendance_ID INT PRIMARY KEY,
+  Student_ID INT,
+  Course_ID INT,
+  Date DATE,
+  Attendance_Status VARCHAR(10),
+  FOREIGN KEY (Student_ID) REFERENCES Students(Student_ID),
+  FOREIGN KEY (Course_ID) REFERENCES Courses(Course_ID)
+);
+
+--Insert Records:
+
+INSERT INTO Students (Student_ID, Name, Email, Phone, Address)
+VALUES
+(1, 'John Doe', 'johndoe@example.com', '1234567890', '123 Main St'),
+(2, 'Jane Smith', 'janesmith@example.com', '9876543210', '456 Elm St'),
+(3, 'Bob Johnson', 'bobjohnson@example.com', '5678901234', '789 Oak St'),
+(4, 'Alice Brown', 'alicebrown@example.com', '2345678901', '321 Maple St'),
+(5, 'Mike Davis', 'mikedavis@example.com', '9012345678', '567 Pine St'),
+(6, 'Emily Taylor', 'emilytaylor@example.com', '6789012345', '901 Cedar St'),
+(7, 'Sarah Lee', 'sarahlee@example.com', '3456789012', '234 Walnut St'),
+(8, 'Kevin White', 'kevinwhite@example.com', '1234890765', '678 Spruce St'),
+(9, 'Lisa Harris', 'lisaharris@example.com', '7654321098', '890 Fir St'),
+(10, 'Tom Jackson', 'tomjackson@example.com', '4567890123', '567 Larch St');
+
+--Courses Table:
+
+INSERT INTO Courses (Course_ID, Course_Name, Credits, Department)
+VALUES
+(1, 'Introduction to Computer Science', 3, 'Computer Science'),
+(2, 'Calculus', 4, 'Mathematics'),
+(3, 'English Literature', 3, 'English'),
+(4, 'Biology', 4, 'Biology'),
+(5, 'Physics', 4, 'Physics'),
+(6, 'Data Structures', 3, 'Computer Science'),
+(7, 'Algorithms', 3, 'Computer Science'),
+(8, 'Operating Systems', 3, 'Computer Science'),
+(9, 'Database Systems', 3, 'Computer Science'),
+(10, 'Software Engineering', 3, 'Computer Science');
+
+--Enrollments Table:
+
+INSERT INTO Enrollments (Enrollment_ID, Student_ID, Course_ID, Enrollment_Date)
+VALUES
+(1, 1, 1, '2022-01-01'),
+(2, 2, 2, '2022-01-05'),
+(3, 3, 3, '2022-01-10'),
+(4, 1, 4, '2022-01-15'),
+(5, 2, 5, '2022-01-20'),
+(6, 3, 6, '2022-01-25'),
+(7, 1, 7, '2022-02-01'),
+(8, 2, 8, '2022-02-05'),
+(9, 3, 9, '2022-02-10'),
+(10, 1, 10, '2022-02-15');
+
+--Grades Table:
+
+INSERT INTO Grades (Grade_ID, Student_ID, Course_ID, Assignment_Name, Grade)
+VALUES
+(1, 1, 1, 'Homework 1', 85.00),
+(2, 2, 2, 'Quiz 1', 90.00),
+(3, 3, 3, 'Midterm Exam', 78.00),
+(4, 1, 4, 'Homework 2', 92.00),
+(5, 2, 5, 'Project', 88.00),
+(6, 3, 6, 'Final Exam', 95.00),
+(7, 1, 7, 'Homework 3', 89.00),
+(8, 2, 8, 'Quiz 2', 91.00),
+(9, 3, 9, 'Group Project', 82.00),
+(10, 1, 10, 'Final Project', 98.00);
+
+INSERT INTO Attendance (Attendance_ID, Student_ID, Course_ID, Date, Attendance_Status) VALUES
+(3, 3, 3, '2022-01-12', 'Present'),
+(4, 1, 1, '2022-01-08', 'Late'),
+(5, 2, 2, '2022-01-15', 'Absent'),
+(6, 3, 6, '2022-02-02', 'Present'),
+(7, 1, 7, '2022-02-10', 'Present'),
+(8, 2, 8, '2022-02-12', 'Late'),
+(9, 3, 9, '2022-02-15', 'Absent'),
+(10, 1, 10, '2022-02-20', 'Present');
+
